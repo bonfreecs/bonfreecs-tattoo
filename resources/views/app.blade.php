2f4fd947f25552e89937c31eed3934b067dd52ee
Irvin <irvin@email.com>
2026-05-05 22:37:58 +0800

    Fix Blade template for production assets

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,10 @@
     <title>BONFREECS Tattoo | Dark Art Studio</title>
     <meta name="description" content="Dark art tattoo studio specializing in custom designs, blackwork, and neo-traditional tattoos.">
     
-    <!-- Favicon with cache busting -->
     <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2.0">
-    <link rel="shortcut icon" href="/favicon.svg?v=2.0">
     
-    @viteReactRefresh()
-    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    <link rel="stylesheet" href="/build/assets/app-B-BEL-Ix.css">
+    <script type="module" src="/build/assets/app-czsZWiAi.js"></script>
 </head>
 <body>
     @inertia
